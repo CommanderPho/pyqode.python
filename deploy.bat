41b6d4386a396b497895afb4b19f4bfcc062238e
Pho Hale <CommanderPho@users.noreply.github.com>
2023-02-07 16:11:38 -0500

    changes

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,10 @@
-python -m pip install --user --upgrade setuptools wheel
+python -m pip install --upgrade setuptools wheel
 
 python setup.py sdist bdist_wheel
 
-python -m pip install --user --upgrade twine
+python -m pip install --upgrade twine
 
-python -m twine upload --repository pypi dist/*
+@REM python -m twine upload --repository pypi dist/*
 
 
 
